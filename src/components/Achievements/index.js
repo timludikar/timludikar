0797d982687395b7bab3384d07da078866903db5
Tim Ludikar <tim.ludikar@rangle.io>
2018-01-24 16:41:59 -0500

    Adding Achievements components

---
+++ src/components/Achievements/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+import Section from "../Section";
+
+const Article = styled.article`
+  margin-bottom: 2%;
+`;
+const Heading = styled.h3`
+  color: lightblue;
+  margin-top: 0;
+  text-transform: uppercase;
+  text-align: left;
+  margin-bottom: 1%;
+`;
+const UnorderedList = styled.ul``;
+const ListItem = styled.li``;
+
+const Achievements = ({ achievements }) => {
+  const achievementCmpt = achievements.map(
+    ({ company, company_achievements }, i) => {
+      const company_achievementsCmpt = company_achievements.map(
+        (cmpy_achievement, i) => (
+          <ListItem key={`achievement-company-${cmpy_achievement}-${i}`}>
+            {cmpy_achievement}
+          </ListItem>
+        )
+      );
+      return (
+        <Article key={`achievements-${company}`}>
+          <Heading>{company}</Heading>
+          <UnorderedList>{company_achievementsCmpt}</UnorderedList>
+        </Article>
+      );
+    }
+  );
+  return <Section title="Achievements">{achievementCmpt}</Section>;
+};
+
+export default Achievements;
--- src/templates/resumeTemplate.js
+++ src/templates/resumeTemplate.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 import Introduction from "../components/Introduction";
+import Achievements from "../components/Achievements";
 import Skill from "../components/Skills";
 
 export default function Template({ data }) {
-  console.log(data);
   const { resumeJson } = data;
-  const { introduction, skills } = resumeJson;
+  const { introduction, skills, achievements } = resumeJson;
   return (
     <div>
       <Introduction introduction={introduction} />
+      <Achievements achievements={achievements} />
       <Skill skills={skills} />
     </div>
   );
@@ -25,6 +26,10 @@ export const pageQuery = graphql`
         competencies
       }
       skills
+      achievements {
+        company
+        company_achievements
+      }
     }
   }
 `;
